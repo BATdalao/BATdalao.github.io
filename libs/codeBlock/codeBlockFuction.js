33f2bee4c7385b7394e46b8fcab990ddd543e45c
BATdalao <55501447+BATdalao@users.noreply.github.com>
2020-09-02 09:51:54 +0800

    Site updated: 2020-09-02 09:51:32

--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,5 +1,3 @@
-// 代码块功能依赖
-
-$(function () {
-    $('pre').wrap('<div class="code-area" style="position: relative"></div>');
-});
+// build time:Wed Sep 02 2020 09:51:17 GMT+0800 (GMT+08:00)
+$(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
+//rebuild by neat 
