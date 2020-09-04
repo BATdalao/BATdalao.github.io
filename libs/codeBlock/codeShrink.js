3be00c009ea78e15c36b1aa45978f0615535bf01
BATdalao <55501447+BATdalao@users.noreply.github.com>
2020-09-04 16:51:44 +0800

    Site updated: 2020-09-04 16:51:37

--- libs/codeBlock/codeShrink.js
+++ libs/codeBlock/codeShrink.js
@@ -1,3 +1,3 @@
-// build time:Fri Sep 04 2020 16:49:42 GMT+0800 (GMT+08:00)
+// build time:Fri Sep 04 2020 16:51:34 GMT+0800 (GMT+08:00)
 $(function(){var e=$('<i class="fas fa-angle-up code-expand" aria-hidden="true"></i>');$(".code-area").prepend(e);$(".code-expand").on("click",function(){if($(this).parent().hasClass("code-closed")){$(this).siblings("pre").find("code").show();$(this).parent().removeClass("code-closed")}else{$(this).siblings("pre").find("code").hide();$(this).parent().addClass("code-closed")}})});
 //rebuild by neat 
