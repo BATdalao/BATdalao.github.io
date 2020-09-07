2ba2afc9a2912e35ea673c5361eedc734553f7a8
BATdalao <55501447+BATdalao@users.noreply.github.com>
2020-09-07 15:09:24 +0800

    Site updated: 2020-09-07 15:09:08

--- libs/codeBlock/codeLang.js
+++ libs/codeBlock/codeLang.js
@@ -1,3 +1,3 @@
-// build time:Mon Sep 07 2020 15:03:46 GMT+0800 (GMT+08:00)
+// build time:Mon Sep 07 2020 15:08:55 GMT+0800 (GMT+08:00)
 $(function(){var e=$('<div class="code_lang" title="代码语言"></div>');$("pre").before(e);$("pre").each(function(){var e=$(this).attr("class");if(!e){return true}var r=e.replace("line-numbers","").trim().replace("language-","").trim();$(this).siblings(".code_lang").text(r)})});
 //rebuild by neat 
